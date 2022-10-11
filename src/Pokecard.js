0d7a46ae07e873438d276574332c49369622e31a
Farhan <farhanmnavas@gmail.com>
2022-10-11 23:49:21 +0800

    Basic Pokedex layout and fancy images used

---
+++ src/Pokecard.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./Pokecard.css"
+
+
+class Pokecard extends Component {
+    render() {
+        const {id, name, type, base_experience} = this.props
+        return (
+            <div className="Pokecard">
+                <h2 className="Pokecard-header">{name}</h2>
+                <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`} alt=""/>
+                <div>type: {type}</div>
+                <div>EXP: {base_experience}</div>
+            </div>
+        )
+    }
+}
+
+export default Pokecard
